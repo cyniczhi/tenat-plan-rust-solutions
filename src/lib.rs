b6b6be4b49bd23b2664ebcad0a222c910f888992
zhizheng <zhizheng0619@gmail.com>
2023-04-25 22:15:57 +0800

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
-use std::collections::HashMap;
+use snafu::{ResultExt, Snafu};
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+};
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Debug, Snafu)]
+enum Error {
+    #[snafu(display("Unable to open file from {}: {}", path.display(), source))]
+    OpenLogFile {
+        source: std::io::Error,
+        path: PathBuf,
+    },
+}
 
 /// This is an implementation of in-memory k-v store
 pub struct KvStore {
@@ -18,15 +33,23 @@ impl KvStore {
         }
     }
 
-    pub fn get(&self, key: String) -> Option<String> {
-        self.store.get(&key).cloned()
+    pub fn open(path: &Path) -> Result<KvStore> {
+        Err("unimplemented".to_string())
     }
 
-    pub fn set(&mut self, key: String, val: String) {
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        Ok(self.store.get(&key).cloned())
+    }
+
+    pub fn set(&mut self, key: String, val: String) -> Result<()> {
         self.store.insert(key, val);
+
+        Err("unimplemented".to_string())
     }
 
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.store.remove(&key);
+
+        Err("noimpl".to_string())
     }
 }
